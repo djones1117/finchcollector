af2ca8e61f4542f02738192f5e260edacf4adf8c
djones1117 <socjones21@yahoo.com>
2023-08-15 12:00:48 -0400

    update and remove finch working

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
 
 
@@ -12,9 +12,7 @@ def about(request):
   return render(request, 'about.html')
 
 def finches_index(request):
-  
   finches = Finch.objects.all()
-  
   return render(request, 'finches/index.html', {
     'finches': finches
   })
@@ -22,3 +20,17 @@ def finches_index(request):
 def finches_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
   return render(request, 'finches/detail.html', {'finch': finch})
+
+class FinchCreate(CreateView):
+  model = Finch
+  fields = '__all__'
+
+
+class FinchUpdate(UpdateView):
+  model = Finch
+  fields = ['breed', 'description', 'age']
+
+
+class FinchDelete(DeleteView):
+  model = Finch
+  success_url = '/finches'
