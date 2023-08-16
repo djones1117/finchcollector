fd03b015b352ee90488d5c9b41b1aaf178543f76
djones1117 <socjones21@yahoo.com>
2023-08-16 16:11:21 -0400

    add toys to toy list, many to many, add toy to finch

--- main_app/models.py
+++ main_app/models.py
@@ -1,12 +1,36 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
+
 # Create your models here.
+
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner'),
+)
+
+# Create your models here.
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('toys_detail', kwargs={'pk': self.id})
+
+
+
 class Finch(models.Model):
 
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
+
+    toys = models.ManyToManyField(Toy)
 
 
 
@@ -17,12 +41,11 @@ class Finch(models.Model):
     def get_absolute_url(self):
         return reverse('detail', kwargs={'finch_id': self.id})
     
+    def fed_for_today(self):
+        return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
+    
 
-MEALS = (
-    ('B', 'Breakfast'),
-    ('L', 'Lunch'),
-    ('D', 'Dinner'),
-)
+
 
 # one finch has many feedings, feeding belongs to a finch
 class Feeding(models.Model):
